27765f6abd402f0adeaff7fe901588929e9584d3
H Goktekin <haticegoktekinn@gmail.com>
2019-08-03 11:54:14 +0300

    first commit

--- Git Test project/ViewController.swift
+++ Git Test project/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("git")
+        print("git2")
         // Do any additional setup after loading the view.
     }
 
