ef5e2f7f5769afabd5bbbc8a093a9a897a83b7f7
H Goktekin <haticegoktekinn@gmail.com>
2019-08-03 11:53:16 +0300

    start commit

---
+++ Git Test project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Git Test project
+//
+//  Created by Hatice Göktekin on 3.08.2019.
+//  Copyright © 2019 Hatice Göktekin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
